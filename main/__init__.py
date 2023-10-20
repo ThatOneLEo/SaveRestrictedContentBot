cde0a6f3cf6f3c850c977f2b0e3c4c9f1ec5f861
ThatOneLEo <73996695+ThatOneLEo@users.noreply.github.com>
2023-10-20 15:07:55 +0500

    __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("28361668", default=None, cast=int)
-API_HASH = config("50b661fce21cd08a19d36a9761263d8b", default=None)
-BOT_TOKEN = config("6791474892:AAHBSjRMz-HT-nbEY1vU0YgOh_2GH5-cnMs", default=None)
-SESSION = config("BACr70_y1MRKfaRqnUWmoTcjj2EQWdUWk-GHQz737uIgH0hRroSPdrgM577uaFuPJscIEgbMGh8qQqoZQulmY8uwZ38omvfmiSuYKKDHx8shZds68gORNXN9ykFhyKoApSGbVoY2rOWl1iqAUKa9tUsNihU423LOYL4OmFzWLnDP8f_XHKd4JbATw8Z9YOha37k8CqEbGpiUyplS-m5xdKryfihhYJSUXF9qo-BJehgDBzbRFhcqJhxjAALSCq4JdPIMYf_vZMwbxvnf7lm-p7f_a3HqmPm-YT6Z5zt84V7ToYJwStMXl5ImO9F6-JemI-wAerX3TUHhQ-wUMOEZaAfBAAAAAXqT6EwA", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("6351480908", default=None, cast=int)
+API_ID = 28361668
+API_HASH = "50b661fce21cd08a19d36a9761263d8b"
+BOT_TOKEN = "6791474892:AAHBSjRMz-HT-nbEY1vU0YgOh_2GH5-cnMs"
+SESSION = "BACr70_y1MRKfaRqnUWmoTcjj2EQWdUWk-GHQz737uIgH0hRroSPdrgM577uaFuPJscIEgbMGh8qQqoZQulmY8uwZ38omvfmiSuYKKDHx8shZds68gORNXN9ykFhyKoApSGbVoY2rOWl1iqAUKa9tUsNihU423LOYL4OmFzWLnDP8f_XHKd4JbATw8Z9YOha37k8CqEbGpiUyplS-m5xdKryfihhYJSUXF9qo-BJehgDBzbRFhcqJhxjAALSCq4JdPIMYf_vZMwbxvnf7lm-p7f_a3HqmPm-YT6Z5zt84V7ToYJwStMXl5ImO9F6-JemI-wAerX3TUHhQ-wUMOEZaAfBAAAAAXqT6EwA"
+FORCESUB = "DroneBots"
+AUTH = 6351480908
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
